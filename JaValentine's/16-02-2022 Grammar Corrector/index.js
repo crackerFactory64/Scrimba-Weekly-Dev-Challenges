a7aeec2431aa64e6659ca28f60889aab23c9a4e2
crackerFactory64 <lee.mander@gmail.com>
2022-02-21 12:50:02 +0000

    minor changes and rename

--- JaValentine's/16-02-2022 Grammar Corrector/index.js
+++ JaValentine's/16-02-2022 Grammar Corrector/index.js
@@ -2,13 +2,12 @@ const paragraph = document.getElementById("paragraph");
 const btn = document.getElementById("btn");
 
 function correct() {
-  let wordArr = paragraph.innerText.split("");
+  const wordArr = paragraph.innerText.toLowerCase().split("");
 
-  wordArr = wordArr.map((letter) => letter.toLowerCase());
   wordArr[0] = wordArr[0].toUpperCase();
 
-  wordArr.forEach((letter, index) => {
-    if (letter === " ") {
+  wordArr.forEach((char, index) => {
+    if (char === " ") {
       wordArr[index + 1] = wordArr[index + 1].toUpperCase();
     }
   });
